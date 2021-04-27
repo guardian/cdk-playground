78e171418436f44613a4a371f5a13ae8376cd970
Akash Askoolum <akash1810@gmail.com>
2021-04-27 08:22:49 +0100

    feat: Update index page to return build info
    We don't need any more information than the build info for this app.

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -1,4 +1,4 @@
-import controllers.{AssetsComponents, HomeController, ManagementController}
+import controllers.{AssetsComponents, ManagementController}
 import play.api.mvc.EssentialFilter
 import play.api.{ApplicationLoader, BuiltInComponentsFromContext}
 import play.api.routing.Router
@@ -13,8 +13,7 @@ class AppComponents(context: ApplicationLoader.Context)
   private val disabledFilters: Set[EssentialFilter] = Set(allowedHostsFilter)
   override def httpFilters: Seq[EssentialFilter] = super.httpFilters.filterNot(disabledFilters.contains)
 
-  lazy val homeController = new HomeController(controllerComponents)
   lazy val managementController = new ManagementController(controllerComponents)
 
-  lazy val router: Router = new Routes(httpErrorHandler, homeController, managementController, assets)
+  lazy val router: Router = new Routes(httpErrorHandler, managementController, assets)
 }
--- build.sbt
+++ build.sbt
@@ -6,7 +6,6 @@ lazy val root = (project in file("."))
     name := """cdk-playground""",
     version := "1.0-SNAPSHOT",
     scalaVersion := "2.13.5",
-    libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test,
     scalacOptions ++= List(
       "-encoding", "utf8",
       "-deprecation",
