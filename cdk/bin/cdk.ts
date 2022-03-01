ad31984690a579878710c0ff6447518d6f34340f
Akash Askoolum <akash1810@gmail.com>
2022-03-01 17:32:03 +0000

    Add support for CDK diffing
    One can now run `./script/diff-cdk` (or `npm run diff`) to compare their local CDK stack with the running instance.
    
    This is useful when testing changes and offers a shorter feedback loop vs. using the AWS web console.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,4 +3,10 @@ import { App } from "@aws-cdk/core";
 import { CdkPlayground } from "../lib/cdk-playground";
 
 const app = new App();
-new CdkPlayground(app, "CdkPlayground", { stack: "deploy" });
+new CdkPlayground(app, "CdkPlayground", {
+  stack: "deploy",
+  cloudFormationStackName: "cdk-playground-PROD",
+  env: {
+    region: "eu-west-1",
+  },
+});
