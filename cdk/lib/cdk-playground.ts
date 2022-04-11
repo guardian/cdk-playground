d349d0f53d869af7b9209843a60d8e21c7e988e7
Akash Askoolum <akash1810@gmail.com>
2022-04-11 13:42:23 +0100

    chore(deps): Update `@guardian/cdk` from v38.2.1 to v39.0.0
    This brings in this breaking change https://github.com/guardian/cdk/releases/tag/v39.0.0.

--- cdk/lib/cdk-playground.ts
+++ cdk/lib/cdk-playground.ts
@@ -1,7 +1,6 @@
 import { InstanceClass, InstanceSize, InstanceType } from "@aws-cdk/aws-ec2";
 import type { App } from "@aws-cdk/core";
 import { GuPlayApp } from "@guardian/cdk";
-import { Stage } from "@guardian/cdk/lib/constants";
 import { AccessScope } from "@guardian/cdk/lib/constants/access";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack, GuStringParameter } from "@guardian/cdk/lib/constructs/core";
@@ -11,10 +10,10 @@ export class CdkPlayground extends GuStack {
   private static app: AppIdentity = {
     app: "cdk-playground",
   };
-  constructor(scope: App, id: string, props: GuStackProps) {
+  constructor(scope: App, id: string, props: Omit<GuStackProps, "stage">) {
     const { app } = CdkPlayground.app;
 
-    super(scope, id, props);
+    super(scope, id, { ...props, stage: "PROD" });
     AppIdentity.taggedConstruct(CdkPlayground.app, this);
 
     const hostedZoneIdParam = new GuStringParameter(this, "HostedZone", {
@@ -32,24 +31,13 @@ export class CdkPlayground extends GuStack {
         },
       },
       certificateProps: {
-        [Stage.CODE]: {
-          domainName: "IGNORED. Just here to satisfy the type checks.",
-        },
-        [Stage.PROD]: {
-          domainName: "cdk-playground.devx.dev-gutools.co.uk",
-          hostedZoneId: hostedZoneIdParam.valueAsString,
-        },
+        domainName: "cdk-playground.devx.dev-gutools.co.uk",
+        hostedZoneId: hostedZoneIdParam.valueAsString,
       },
       monitoringConfiguration: { noMonitoring: true },
       scaling: {
-        [Stage.CODE]: {
-          minimumInstances: 0,
-          maximumInstances: 0,
-        },
-        [Stage.PROD]: {
-          minimumInstances: 1,
-          maximumInstances: 2,
-        },
+        minimumInstances: 1,
+        maximumInstances: 2,
       },
     });
   }
