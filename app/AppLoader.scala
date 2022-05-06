6e5b877693f86d97785971ac4c6c8706d74b2ff4
Akash Askoolum <akash1810@gmail.com>
2022-05-06 10:54:37 +0100

    fix: Setup logging within the custom application logger
    See https://www.playframework.com/documentation/2.8.x/SettingsLogger#Using-a-custom-application-loader

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,9 +1,11 @@
 import play.api._
+import play.api.libs.logback.LogbackLoggerConfigurator
 
 class AppLoader extends ApplicationLoader {
   private var components: AppComponents = _
 
   def load(context: ApplicationLoader.Context): Application = {
+    new LogbackLoggerConfigurator().configure(context.environment)
     components = new AppComponents(context)
     components.application
   }
