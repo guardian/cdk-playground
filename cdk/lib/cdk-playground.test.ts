41b24b316f8b28680e282d394aa2b3da22a15b38
Akash Askoolum <akash1810@gmail.com>
2022-07-07 19:55:32 +0100

    test: Add test for generated `riff-raff.yaml`

--- cdk/lib/cdk-playground.test.ts
+++ cdk/lib/cdk-playground.test.ts
@@ -1,11 +1,18 @@
+import { RiffRaffYamlFile } from "@guardian/cdk/lib/utils/riff-raff-yaml-file";
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import { CdkPlayground } from "./cdk-playground";
 
-describe("The Deploy stack", () => {
+describe("The CdkPlayground stack", () => {
   it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new CdkPlayground(app, "CdkPlayground", { stack: "deploy" });
+    const app = new App({ outdir: "/tmp/cdk.out" });
+    const stack = new CdkPlayground(app, "CdkPlayground", {
+      stack: "deploy",
+      env: { region: "eu-west-1" },
+    });
     expect(Template.fromStack(stack).toJSON()).toMatchSnapshot();
+
+    const riffRaffYaml = new RiffRaffYamlFile(app);
+    expect(riffRaffYaml.toYAML()).toMatchSnapshot();
   });
 });
