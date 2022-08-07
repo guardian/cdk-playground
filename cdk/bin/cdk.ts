ac5508116ad917741d1498d39f0f3ae30dd2954c
Akash Askoolum <akash1810@gmail.com>
2022-08-07 12:09:28 +0100

    chore(deps): Bump dependencies within `cdk`
    Also inline some config into `package.json`.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,6 @@
-import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { CdkPlayground } from "../lib/cdk-playground";
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { CdkPlayground } from '../lib/cdk-playground';
 
 const app = new App();
-new CdkPlayground(app, "CdkPlayground", { stack: "deploy" });
+new CdkPlayground(app, 'CdkPlayground', { stack: 'deploy' });
