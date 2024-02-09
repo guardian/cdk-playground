d588a7170e81c8b9996bce4c4776ef3f75661c34
akash1810 <akash1810@users.noreply.github.com>
2024-02-09 13:47:40 +0000

    feat: Introduce cdk-nag
    See https://aws.amazon.com/blogs/devops/manage-application-security-and-compliance-with-the-aws-cloud-development-kit-and-cdk-nag/.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,9 @@
 import 'source-map-support/register';
 import { GuRoot } from "@guardian/cdk/lib/constructs/root";
+import {Aspects} from "aws-cdk-lib";
+import { AwsSolutionsChecks } from 'cdk-nag'
 import { CdkPlayground } from '../lib/cdk-playground';
 
 const app = new GuRoot();
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 new CdkPlayground(app, 'CdkPlayground');
