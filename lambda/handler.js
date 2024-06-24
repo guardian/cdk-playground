51dc310b9300131c021385b748f5c150a8de0b13
akash1810 <akash1810@users.noreply.github.com>
2024-06-24 13:24:42 +0100

    Log in multiple formats to see how Central ELK ingests them

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,6 +1,10 @@
 exports.main = async () => {
   const message = `Welcome to CDK-Playground. The time is ${new Date().toString()}`;
   console.log(message);
+  console.log("This is a plain text message");
+  console.log({ message: "This is a JSON message with markers", status: 200 });
+  console.log(JSON.stringify({ message: "This is a JSON.stringify message with markers", status: 200 }));
+  console.log("This is a plain text message with markers", { status: 200 });
   return {
     statusCode: 200,
     body: message
