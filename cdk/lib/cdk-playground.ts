b6ecd615625e33b83c836fefd697372a6503043f
akash1810 <akash1810@users.noreply.github.com>
2024-08-15 05:08:06 +0100

    wip: Use an `AutoScalingRollingUpdate` policy

--- cdk/lib/cdk-playground.ts
+++ cdk/lib/cdk-playground.ts
@@ -7,7 +7,15 @@ import { GuCname } from '@guardian/cdk/lib/constructs/dns';
 import { GuFastlyLogsIamRole } from '@guardian/cdk/lib/constructs/iam';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
-import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import type { CfnAutoScalingGroup } from 'aws-cdk-lib/aws-autoscaling';
+import { UpdatePolicy } from 'aws-cdk-lib/aws-autoscaling';
+import {
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	UserData,
+} from 'aws-cdk-lib/aws-ec2';
+import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkPlayground extends GuStack {
@@ -26,30 +34,99 @@ export class CdkPlayground extends GuStack {
 		const ec2App = 'cdk-playground';
 		const ec2AppDomainName = 'cdk-playground.gutools.co.uk';
 
-		const { loadBalancer } = new GuPlayApp(this, {
-			app: ec2App,
-			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
-			access: { scope: AccessScope.PUBLIC },
-			userData: {
-				distributable: {
-					fileName: `${ec2App}.deb`,
-					executionStatement: `dpkg -i /${ec2App}/${ec2App}.deb`,
+		const minimumInstances = 1;
+
+		const { loadBalancer, autoScalingGroup, targetGroup } = new GuPlayApp(
+			this,
+			{
+				app: ec2App,
+				instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
+				access: { scope: AccessScope.PUBLIC },
+				userData: {
+					distributable: {
+						fileName: `${ec2App}.deb`,
+						executionStatement: `dpkg -i /${ec2App}/${ec2App}.deb`,
+					},
 				},
+				certificateProps: {
+					domainName: ec2AppDomainName,
+				},
+				monitoringConfiguration: { noMonitoring: true },
+				scaling: {
+					minimumInstances,
+				},
+				applicationLogging: {
+					enabled: true,
+					systemdUnitName: 'cdk-playground',
+				},
+				imageRecipe: 'developerPlayground-arm64-java11',
+				updatePolicy: UpdatePolicy.rollingUpdate({
+					maxBatchSize: minimumInstances,
+					minInstancesInService: minimumInstances,
+					minSuccessPercentage: 100,
+					waitOnResourceSignals: true,
+					suspendProcesses: [],
+				}),
 			},
-			certificateProps: {
-				domainName: ec2AppDomainName,
-			},
-			monitoringConfiguration: { noMonitoring: true },
-			scaling: {
-				minimumInstances: 1,
-				maximumInstances: 2,
-			},
-			applicationLogging: {
-				enabled: true,
-				systemdUnitName: 'cdk-playground',
-			},
-			imageRecipe: 'developerPlayground-arm64-java11',
-		});
+		);
+
+		const { role, userData } = autoScalingGroup;
+		const { stackId, region } = this;
+
+		new Policy(this, 'AsgRollingUpdatePolicy', {
+			statements: [
+				// Allow usage of command `cfn-signal`.
+				new PolicyStatement({
+					actions: ['cloudformation:SignalResource'],
+					effect: Effect.ALLOW,
+					resources: [stackId],
+				}),
+
+				/*
+        Allow usage of command `aws elbv2 describe-target-health`.
+        AWS Elastic Load Balancing does not support resource based policies, so the resource has to be `*` (any) here.
+        See https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_aws-services-that-work-with-iam.html
+         */
+				new PolicyStatement({
+					actions: ['elasticloadbalancing:DescribeTargetHealth'],
+					effect: Effect.ALLOW,
+					resources: ['*'],
+				}),
+			],
+		}).attachToRole(role);
+
+		userData.addCommands(
+			`
+      INSTANCE_ID=$(ec2metadata --instance-id)
+
+      STATE=$(aws elbv2 describe-target-health \
+        --target-group-arn ${targetGroup.targetGroupArn} \
+        --region ${region} \
+        --targets Id=$INSTANCE_ID,Port=${GuPlayApp.PORT} \
+        --query "TargetHealthDescriptions[0].TargetHealth.State")
+
+      until [ "$STATE" == "\\"healthy\\"" ]; do
+        echo "Instance not yet healthy within target group. Current state $STATE. Sleeping..."
+        sleep 5
+        STATE=$(aws elbv2 describe-target-health \
+          --target-group-arn ${targetGroup.targetGroupArn} \
+          --region ${region} \
+          --targets Id=$INSTANCE_ID,Port=${GuPlayApp.PORT} \
+          --query "TargetHealthDescriptions[0].TargetHealth.State")
+      done
+      `,
+		);
+
+		const cfnAsg = autoScalingGroup.node.defaultChild as CfnAutoScalingGroup;
+
+		userData.addOnExitCommands(
+			`
+        cfn-signal --stack ${stackId} \
+          --resource ${cfnAsg.logicalId} \
+          --region ${region} \
+          --exit-code $exitCode || echo 'Failed to send Cloudformation Signal'
+        `,
+		);
 
 		new GuCname(this, 'EC2AppDNS', {
 			app: ec2App,
