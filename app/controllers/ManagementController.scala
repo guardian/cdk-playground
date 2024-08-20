64a276406b8424aa4a7979ed0df025ff26ae32e4
akash1810 <akash1810@users.noreply.github.com>
2024-08-20 13:46:16 +0100

    wip

--- app/controllers/ManagementController.scala
+++ app/controllers/ManagementController.scala
@@ -16,6 +16,7 @@ class ManagementController (override val controllerComponents: ControllerCompone
 
   def healthCheck: Action[AnyContent] = Action {
     logger.info("hello from the health check")
+//    Ok("ok")
     InternalServerError
   }
 
