8105799e7e22f97deaadc3adbf88295de75d1859
akash1810 <akash1810@users.noreply.github.com>
2024-11-18 15:52:07 +0000

    feat: Configure AWS CloudWatch Agent to collect instance metrics

--- cdk/lib/cdk-playground.ts
+++ cdk/lib/cdk-playground.ts
@@ -1,15 +1,26 @@
+import path from 'node:path';
 import { GuApiLambda } from '@guardian/cdk';
 import { AccessScope } from '@guardian/cdk/lib/constants/access';
 import { GuCertificate } from '@guardian/cdk/lib/constructs/acm';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
-import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import {
+	GuDistributionBucketParameter,
+	GuStack,
+} from '@guardian/cdk/lib/constructs/core';
 import { GuCname } from '@guardian/cdk/lib/constructs/dns';
+import { GuAllowPolicy } from '@guardian/cdk/lib/constructs/iam';
 import { GuEc2AppExperimental } from '@guardian/cdk/lib/experimental/patterns/ec2-app';
 import type { App } from 'aws-cdk-lib';
 import { CfnOutput, Duration } from 'aws-cdk-lib';
 import { CfnScalingPolicy } from 'aws-cdk-lib/aws-autoscaling';
-import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import {
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	UserData,
+} from 'aws-cdk-lib/aws-ec2';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 interface CdkPlaygroundProps extends Omit<GuStackProps, 'stack' | 'stage'> {
 	/**
@@ -32,9 +43,51 @@ export class CdkPlayground extends GuStack {
 		});
 
 		const { buildIdentifier } = props;
+		const { stack, stage } = this;
 
 		const ec2App = 'cdk-playground';
 		const ec2AppDomainName = 'cdk-playground.gutools.co.uk';
+
+		const distBucket = Bucket.fromBucketAttributes(this, 'DistributionBucket', {
+			bucketName: GuDistributionBucketParameter.getInstance(this).valueAsString,
+		});
+
+		const userData = UserData.forLinux();
+
+		// Download application from S3
+		userData.addS3DownloadCommand({
+			bucket: distBucket,
+			bucketKey: path.join(
+				stack,
+				stage,
+				ec2App,
+				`${ec2App}-${buildIdentifier}.deb`,
+			),
+			localFile: `/${ec2App}/${ec2App}-${buildIdentifier}.deb`,
+		});
+
+		// Install and run application
+		userData.addCommands(`dpkg -i /${ec2App}/${ec2App}-${buildIdentifier}.deb`);
+
+		// Download the CloudWatch Agent configuration from S3
+		const cloudwatchAgentConfigPath =
+			'/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json';
+		userData.addS3DownloadCommand({
+			bucket: distBucket,
+			bucketKey: path.join(
+				stack,
+				stage,
+				ec2App,
+				'amazon-cloudwatch-agent.json',
+			),
+			localFile: cloudwatchAgentConfigPath,
+		});
+
+		// Start the CloudWatch Agent service
+		userData.addCommands(
+			`amazon-cloudwatch-agent-ctl -a fetch-config -c file:${cloudwatchAgentConfigPath}`,
+			'amazon-cloudwatch-agent-ctl -a start',
+		);
 
 		const { loadBalancer, autoScalingGroup } = new GuEc2AppExperimental(this, {
 			buildIdentifier,
@@ -42,12 +95,7 @@ export class CdkPlayground extends GuStack {
 			app: ec2App,
 			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
 			access: { scope: AccessScope.PUBLIC },
-			userData: {
-				distributable: {
-					fileName: `${ec2App}-${buildIdentifier}.deb`,
-					executionStatement: `dpkg -i /${ec2App}/${ec2App}-${buildIdentifier}.deb`,
-				},
-			},
+			userData,
 			certificateProps: {
 				domainName: ec2AppDomainName,
 			},
@@ -61,6 +109,14 @@ export class CdkPlayground extends GuStack {
 				systemdUnitName: 'cdk-playground',
 			},
 			imageRecipe: 'developerPlayground-arm64-java11',
+			roleConfiguration: {
+				additionalPolicies: [
+					new GuAllowPolicy(this, 'CloudWatchAgent', {
+						actions: ['cloudwatch:PutMetricData'],
+						resources: ['*'],
+					}),
+				],
+			},
 		});
 
 		const scaleOutPolicy = new CfnScalingPolicy(autoScalingGroup, 'ScaleOut', {
