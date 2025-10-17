b2be05651c321d53d496f535c263ce305eada142
Jacob Winch <jacob.winch@guardian.co.uk>
2025-10-17 09:33:53 +0100

    Stop hardcoding the MinInstancesInServiceForcdkplayground parameter

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,9 @@
 import 'source-map-support/register';
-import { RiffRaffYamlFile } from '@guardian/cdk/lib/riff-raff-yaml-file';
-import { App } from 'aws-cdk-lib';
+import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { CdkPlayground } from '../lib/cdk-playground';
 import { EventForwarder } from '../lib/event-forwarder';
 
-const app = new App();
+const app = new GuRoot();
 
 const eventForwarder = new EventForwarder(app, 'EventForwarder-CODE');
 
@@ -15,37 +14,3 @@ const applicationStack = new CdkPlayground(app, 'CdkPlayground-CODE', {
 
 // Configure Riff-Raff to deploy the application stack after the EventForwarder stack has finished.
 applicationStack.addDependency(eventForwarder);
-
-const riffRaff = new RiffRaffYamlFile(app);
-const cfnDeployment = riffRaff.riffRaffYaml.deployments.get(
-	'cfn-eu-west-1-deploy-cdk-playground',
-);
-
-/*
-Required only for the initial deployment.
-
-The cdk-playground stack has a CFN parameter `MinInstancesInServiceForcdkplayground` that does not have a default value.
-We have to provide the initial value.
-
-There are a few ways to do this:
-- Create the CFN stack manually in the AWS console and provide the value
-- Set the CFN parameter `riff-raff.yaml` and use Riff-Raff to deploy the stack
-- Set the value in the cdk-playground stack
-
-The latter is tricky, as the parameter is added via an Aspect, which is not executed within the `constructor` of CdkPlayground,
-we'd have to find the relevant CDK hook.
-
-Below, we set the value in the `riff-raff.yaml` file.
- */
-if (cfnDeployment) {
-	cfnDeployment.parameters = {
-		...cfnDeployment.parameters,
-		templateStageParameters: {
-			CODE: {
-				MinInstancesInServiceForcdkplayground: '1',
-			},
-		},
-	};
-}
-
-riffRaff.synth();
