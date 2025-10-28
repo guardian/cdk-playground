d4158e129600f20988d4550e662d7a906cee4b1c
Jacob Winch <jacob.winch@guardian.co.uk>
2025-10-28 08:54:47 +0000

    Add temporary workaround for riff-raff.yaml bug

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,10 @@
 import 'source-map-support/register';
-import { GuRoot } from '@guardian/cdk/lib/constructs/root';
+import { RiffRaffYamlFile } from '@guardian/cdk/lib/riff-raff-yaml-file';
+import { App } from 'aws-cdk-lib';
 import { CdkPlayground } from '../lib/cdk-playground';
 import { EventForwarder } from '../lib/event-forwarder';
 
-const app = new GuRoot();
+const app = new App();
 
 const eventForwarder = new EventForwarder(app, 'EventForwarder-CODE');
 
@@ -14,3 +15,21 @@ const applicationStack = new CdkPlayground(app, 'CdkPlayground-CODE', {
 
 // Configure Riff-Raff to deploy the application stack after the EventForwarder stack has finished.
 applicationStack.addDependency(eventForwarder);
+
+const riffRaff = new RiffRaffYamlFile(app);
+const cfnDeployment = riffRaff.riffRaffYaml.deployments.get(
+	'cfn-eu-west-1-deploy-cdk-playground',
+);
+
+// This is temporarily required to work around a bug with the riff-raff.yaml generation
+// in GuCDK: https://trello.com/c/4sUs6fWY/546-new-deployment-mechanism-fix-bugs-with-riff-raffyaml-generation
+if (cfnDeployment) {
+	cfnDeployment.parameters = {
+		...cfnDeployment.parameters,
+		minInstancesInServiceParameters: {
+			MinInstancesInServiceForcdkplayground: { App: 'cdk-playground' },
+		},
+	};
+}
+
+riffRaff.synth();
