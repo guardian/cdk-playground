d5e34d6d2fd60c471e182eb4d783895cc3baa7b2
akash1810 <akash1810@users.noreply.github.com>
2026-03-12 12:40:10 +0000

    Revert "Add temporary workaround for riff-raff.yaml bug"
    This reverts commit d4158e129600f20988d4550e662d7a906cee4b1c.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,9 @@
 import 'source-map-support/register';
-import { RiffRaffYamlFile } from '@guardian/cdk/lib/riff-raff-yaml-file';
-import { App } from 'aws-cdk-lib';
+import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { CdkPlayground } from '../lib/cdk-playground';
 import { EventForwarder } from '../lib/event-forwarder';
 
-const app = new App();
+const app = new GuRoot();
 
 const eventForwarder = new EventForwarder(app, 'EventForwarder-CODE');
 
@@ -15,21 +14,3 @@ const applicationStack = new CdkPlayground(app, 'CdkPlayground-CODE', {
 
 // Configure Riff-Raff to deploy the application stack after the EventForwarder stack has finished.
 applicationStack.addDependency(eventForwarder);
-
-const riffRaff = new RiffRaffYamlFile(app);
-const cfnDeployment = riffRaff.riffRaffYaml.deployments.get(
-	'cfn-eu-west-1-deploy-cdk-playground',
-);
-
-// This is temporarily required to work around a bug with the riff-raff.yaml generation
-// in GuCDK: https://trello.com/c/4sUs6fWY/546-new-deployment-mechanism-fix-bugs-with-riff-raffyaml-generation
-if (cfnDeployment) {
-	cfnDeployment.parameters = {
-		...cfnDeployment.parameters,
-		minInstancesInServiceParameters: {
-			MinInstancesInServiceForcdkplayground: { App: 'cdk-playground' },
-		},
-	};
-}
-
-riffRaff.synth();
