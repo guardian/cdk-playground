3651d2bb15e4a0acf344c1974a4ca63a55a0cefa
Jacob Winch <jacob.winch@guardian.co.uk>
2026-04-13 12:51:31 +0100

    Drop event forwarder

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,16 +1,10 @@
 import 'source-map-support/register';
 import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { CdkPlayground } from '../lib/cdk-playground';
-import { EventForwarder } from '../lib/event-forwarder';
 
 const app = new GuRoot();
 
-const eventForwarder = new EventForwarder(app, 'EventForwarder-CODE');
-
-const applicationStack = new CdkPlayground(app, 'CdkPlayground-CODE', {
+new CdkPlayground(app, 'CdkPlayground-CODE', {
 	cloudFormationStackName: 'deploy-CODE-cdk-playground',
 	buildIdentifier: process.env.GITHUB_RUN_NUMBER ?? 'DEV',
 });
-
-// Configure Riff-Raff to deploy the application stack after the EventForwarder stack has finished.
-applicationStack.addDependency(eventForwarder);
