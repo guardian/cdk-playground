5010455d8691a1699d0f96b134d1bfbf5b98c704
Jacob Winch <jacob.winch@guardian.co.uk>
2026-05-12 13:44:38 +0100

    Add GuEcsApp in migration mode

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -12,6 +12,7 @@ const eventForwarder = new EventForwarder(app, 'EventForwarder-CODE');
 const ec2Stack = new CdkPlaygroundEc2(app, 'CdkPlaygroundEc2-CODE', {
 	cloudFormationStackName: 'deploy-CODE-cdk-playground-ec2',
 	buildIdentifier: process.env.GITHUB_RUN_NUMBER ?? 'DEV',
+	imageIdentifier: process.env.IMAGE_DIGEST ?? 'DEV',
 });
 
 new CdkPlaygroundLambda(app, 'CdkPlaygroundLambda-CODE', {
--- cdk/lib/cdk-playground-ec2.ts
+++ cdk/lib/cdk-playground-ec2.ts
@@ -2,6 +2,8 @@ import { AccessScope } from '@guardian/cdk/lib/constants/access';
 import { GuStack, type GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuCname } from '@guardian/cdk/lib/constructs/dns';
 import { GuEc2AppExperimental } from '@guardian/cdk/lib/experimental/patterns/ec2-app';
+import { GuEcsAppExperimental } from '@guardian/cdk/lib/experimental/patterns/ecs-app';
+import type { GuDomainName } from '@guardian/cdk/lib/types';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
 import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
@@ -15,6 +17,13 @@ interface CdkPlaygroundEc2Props extends Omit<GuStackProps, 'stack' | 'stage'> {
 	 * process.env.GITHUB_RUN_NUMBER
 	 */
 	buildIdentifier: string;
+	/**
+	 * Which image to run.
+	 * This should be the image digest (e.g. 'sha256:abc123') to ensure immutable deployments.
+	 *
+	 * @see https://docs.docker.com/dhi/core-concepts/digests
+	 */
+	imageIdentifier: string;
 }
 
 export class CdkPlaygroundEc2 extends GuStack {
@@ -26,27 +35,28 @@ export class CdkPlaygroundEc2 extends GuStack {
 			env: { region: 'eu-west-1' },
 		});
 
-		const { buildIdentifier } = props;
+		const { buildIdentifier, imageIdentifier } = props;
 
 		const ec2AppDomainName = 'cdk-playground.code.dev-gutools.co.uk';
+		const certificateProps: GuDomainName = {
+			domainName: ec2AppDomainName,
+		};
 
-		const ec2App = 'cdk-playground';
+		const app = 'cdk-playground';
 
-		const { loadBalancer } = new GuEc2AppExperimental(this, {
+		const ec2App = new GuEc2AppExperimental(this, {
 			buildIdentifier,
 			applicationPort: 9000,
-			app: ec2App,
+			app,
 			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
 			access: { scope: AccessScope.PUBLIC },
 			userData: {
 				distributable: {
-					fileName: `${ec2App}-${buildIdentifier}.deb`,
-					executionStatement: `dpkg -i /${ec2App}/${ec2App}-${buildIdentifier}.deb`,
+					fileName: `${app}-${buildIdentifier}.deb`,
+					executionStatement: `dpkg -i /${app}/${app}-${buildIdentifier}.deb`,
 				},
 			},
-			certificateProps: {
-				domainName: ec2AppDomainName,
-			},
+			certificateProps,
 			monitoringConfiguration: { noMonitoring: true },
 			scaling: {
 				minimumInstances: 1,
@@ -60,11 +70,32 @@ export class CdkPlaygroundEc2 extends GuStack {
 			instanceMetricGranularity: '5Minute',
 		});
 
+		new GuEcsAppExperimental(this, {
+			app,
+			applicationPort: 9000,
+			certificateProps,
+			cpu: 1024,
+			imageIdentifier,
+			memoryLimitMiB: 2048,
+			repositoryName: 'guardian/cdk-playground',
+			scaling: { minimumTasks: 1, maximumTasks: 10 },
+			// This is the important bit for the migration; it allows us to share the existing ALB & listener and split
+			// traffic between the EC2 and ECS target groups
+			migrationProps: {
+				loadBalancer: ec2App.loadBalancer,
+				listener: ec2App.listener,
+				targetGroup: ec2App.targetGroup,
+				// Although to begin with we send 0% traffic to the ECS target group; I think we need an initial deploy to
+				// get some healthy tasks before we start routing traffic to them
+				weightForEcsTargetGroup: 0,
+			},
+		});
+
 		new GuCname(this, 'EC2AppDNS', {
-			app: ec2App,
+			app: app,
 			ttl: Duration.hours(1),
 			domainName: ec2AppDomainName,
-			resourceRecord: loadBalancer.loadBalancerDnsName,
+			resourceRecord: ec2App.loadBalancer.loadBalancerDnsName,
 		});
 	}
 }
