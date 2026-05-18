a8c7e30ac560986ba19d569420dda02f9510adbb
Jacob Winch <jacob.winch@guardian.co.uk>
2026-05-18 16:46:20 +0100

    Add ECS infrastructure with migration helper

--- cdk/lib/cdk-playground-ec2.ts
+++ cdk/lib/cdk-playground-ec2.ts
@@ -1,8 +1,26 @@
+import { AccessScope } from '@guardian/cdk/lib/constants';
 import { GuStack, type GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import {
+	GuLoadBalancedAppExperimental,
+	RouteTrafficToEcsExperimental,
+} from '@guardian/cdk/lib/experimental/patterns/gu-load-balanced-app';
 import type { App } from 'aws-cdk-lib';
+import type {
+	CfnListener,
+	CfnLoadBalancer,
+	CfnTargetGroup,
+} from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
-type CdkPlaygroundEc2Props = Omit<GuStackProps, 'stack' | 'stage'>;
+interface CdkPlaygroundEc2Props extends Omit<GuStackProps, 'stack' | 'stage'> {
+	/**
+	 * Which image to run.
+	 * This should be the image digest (e.g. 'sha256:abc123') to ensure immutable deployments.
+	 *
+	 * @see https://docs.docker.com/dhi/core-concepts/digests
+	 */
+	imageIdentifier: string;
+}
 
 export class CdkPlaygroundEc2 extends GuStack {
 	constructor(scope: App, id: string, props: CdkPlaygroundEc2Props) {
@@ -12,8 +30,41 @@ export class CdkPlaygroundEc2 extends GuStack {
 			stage: 'CODE',
 			env: { region: 'eu-west-1' },
 		});
-		new CfnInclude(this, 'Template', {
+		const yamlStack = new CfnInclude(this, 'Template', {
 			templateFile: '../ec2-app.yaml',
+		});
+		const { imageIdentifier } = props;
+
+		const ec2AppDomainName = 'cdk-playground.code.dev-gutools.co.uk';
+
+		const ec2App = 'cdk-playground';
+
+		const ecsPattern = new GuLoadBalancedAppExperimental(this, {
+			applicationPort: 9000,
+			app: ec2App,
+			access: { scope: AccessScope.PUBLIC },
+			certificateProps: {
+				domainName: ec2AppDomainName,
+			},
+			monitoringConfiguration: { noMonitoring: true },
+			ecsProps: {
+				cpu: 1024,
+				imageIdentifier,
+				memoryLimitMiB: 2048,
+				repositoryName: 'guardian/cdk-playground',
+				scaling: { minimumTasks: 1, maximumTasks: 10 },
+			},
+		});
+
+		new RouteTrafficToEcsExperimental(this, {
+			currentLoadBalancer: yamlStack.getResource(
+				'LoadBalancer',
+			) as CfnLoadBalancer,
+			currentListener: yamlStack.getResource('Listener') as CfnListener,
+			ec2TargetGroup: yamlStack.getResource('Ec2TargetGroup') as CfnTargetGroup,
+			ecsPattern,
+			ec2Weight: 999,
+			ecsWeight: 0,
 		});
 	}
 }
