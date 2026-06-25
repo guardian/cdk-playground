368218d1790336c4316ac1b98c2d72056bc6bc9d
akash1810 <akash1810@users.noreply.github.com>
2026-06-25 11:37:02 +0100

    Revert "feat: Delete ECS-only infrastructure"
    This reverts commit 3662b55d1d08c5f4f4a296a9fd421c6f77c772d6 as we still need a way to deterministically have ECS serve a request.
    Until we implement header based routing, its easiest to have a dedicated load balancer for this.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,7 @@
 import 'source-map-support/register';
 import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { CdkPlayground } from '../lib/cdk-playground';
+import { CdkPlaygroundEcs } from '../lib/cdk-playground-ecs';
 import { CdkPlaygroundLambda } from '../lib/cdk-playground-lambda';
 import { EventForwarder } from '../lib/event-forwarder';
 
@@ -21,6 +22,12 @@ const playground = new CdkPlayground(app, 'CdkPlaygroundEc2-CODE', {
 
 new CdkPlaygroundLambda(app, 'CdkPlaygroundLambda-CODE', {
 	cloudFormationStackName: 'deploy-CODE-cdk-playground-lambda',
+	riffRaffProjectName,
+});
+
+new CdkPlaygroundEcs(app, 'CdkPlaygroundEcs-CODE', {
+	cloudFormationStackName: 'deploy-CODE-cdk-playground-ecs',
+	imageIdentifier: process.env.IMAGE_DIGEST ?? 'DEV',
 	riffRaffProjectName,
 });
 
---
+++ cdk/lib/cdk-playground-ecs.ts
@@ -0,0 +1,60 @@
+import { AccessScope } from '@guardian/cdk/lib/constants/access';
+import { GuStack, type GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuCname } from '@guardian/cdk/lib/constructs/dns';
+import { GuLoadBalancedAppExperimental } from '@guardian/cdk/lib/experimental/patterns/gu-load-balanced-app';
+import type { App } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+
+interface CdkPlaygroundEcsProps extends Omit<GuStackProps, 'stack' | 'stage'> {
+	/**
+	 * Which image to run.
+	 * This should be the image digest (e.g. 'sha256:abc123') to ensure immutable deployments.
+	 *
+	 * @see https://docs.docker.com/dhi/core-concepts/digests
+	 */
+	imageIdentifier: string;
+}
+
+// For now, we provision all of this infrastructure via constructs as part of this repo.
+// Once we've figured out the details we'd aim to provide this to users via a GuCDK pattern instead.
+export class CdkPlaygroundEcs extends GuStack {
+	constructor(scope: App, id: string, props: CdkPlaygroundEcsProps) {
+		super(scope, id, {
+			...props,
+			stack: 'deploy',
+			stage: 'CODE',
+			env: { region: 'eu-west-1' },
+		});
+
+		const { imageIdentifier } = props;
+		const app = 'cdk-playground';
+		const domainName = 'cdk-playground-ecs.code.dev-gutools.co.uk';
+
+		const { loadBalancer } = new GuLoadBalancedAppExperimental(this, {
+			access: { scope: AccessScope.PUBLIC },
+			app,
+			applicationPort: 9000,
+			certificateProps: {
+				domainName,
+			},
+			monitoringConfiguration: { noMonitoring: true },
+			ecsProps: {
+				imageIdentifier,
+				memoryLimitMiB: 2048,
+				cpu: 1024,
+				repositoryName: 'guardian/cdk-playground',
+				scaling: {
+					minimumTasks: 1,
+					maximumTasks: 2,
+				},
+			},
+		});
+
+		new GuCname(this, 'EcsDns', {
+			app,
+			ttl: Duration.minutes(1),
+			domainName,
+			resourceRecord: loadBalancer.loadBalancerDnsName,
+		});
+	}
+}
