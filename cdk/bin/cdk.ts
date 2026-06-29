6a731212b38924aa31a7c77cc01a4d54418db949
akash1810 <akash1810@users.noreply.github.com>
2026-06-29 08:58:26 +0100

    feat!: Delete EC2-only CloudFormation stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,5 @@
 import 'source-map-support/register';
 import { GuRoot } from '@guardian/cdk/lib/constructs/root';
-import { CdkPlaygroundEc2 } from '../lib/cdk-playground-ec2';
 import { CdkPlaygroundEcs } from '../lib/cdk-playground-ecs';
 import { CdkPlaygroundLambda } from '../lib/cdk-playground-lambda';
 import { EventForwarder } from '../lib/event-forwarder';
@@ -9,13 +8,7 @@ const riffRaffProjectName = 'devx::cdk-playground';
 
 const app = new GuRoot();
 
-const eventForwarder = new EventForwarder(app, 'EventForwarder-CODE', {
-	riffRaffProjectName,
-});
-
-const ec2Stack = new CdkPlaygroundEc2(app, 'CdkPlaygroundEc2-CODE', {
-	cloudFormationStackName: 'deploy-CODE-cdk-playground-ec2',
-	buildIdentifier: process.env.GITHUB_RUN_NUMBER ?? 'DEV',
+new EventForwarder(app, 'EventForwarder-CODE', {
 	riffRaffProjectName,
 });
 
@@ -29,6 +22,3 @@ new CdkPlaygroundEcs(app, 'CdkPlaygroundEcs-CODE', {
 	imageIdentifier: process.env.IMAGE_DIGEST ?? 'DEV',
 	riffRaffProjectName,
 });
-
-// Configure Riff-Raff to deploy the application stack after the EventForwarder stack has finished.
-ec2Stack.addDependency(eventForwarder);
