d7ab43bbb1cf1c22fbb8918c7661daef43e5c957
Connor OMalley <connor.omalley@guardian.co.uk>
2026-08-26 10:46:00 +0100

    Use goreplay 1.3.2

--- cdk/lib/HttpTrafficMirroring.ts
+++ cdk/lib/HttpTrafficMirroring.ts
@@ -215,7 +215,7 @@ export class HttpTrafficMirroring extends Construct {
 		});
 
 		taskDefinition.addContainer('MirroringHandlerContainer', {
-			image: ContainerImage.fromRegistry('jauderho/goreplay:1.3.3'),
+			image: ContainerImage.fromRegistry('jauderho/goreplay:v1.3.2'),
 			logging: LogDriver.awsLogs({
 				streamPrefix: 'mirroring-handler',
 				logGroup: handlerLogGroup,
